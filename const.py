bdb57621d0b1ca3cce7f2de5ddf8ebea839f60ae
CallMeHein <heinrichmakovenko@gmail.com>
2024-10-31 11:14:03 +0100

    refactor isIgnorableBlock

--- const.py
+++ const.py
@@ -3,23 +3,34 @@ FULL_STOP = False
 MAX_RAY_STEPS = 20
 STEP_HEIGHT_MIN = 1
 
+__partialBlockNames = [
+    "vine",
+    "obsidian",
+    "nether",
+    "portal",
+    "_grass",
+    "fern",
+    "leaves",
+    "_log"
+]
+
+
+def __checkMoreSpecificConditions(name):
+    return any([
+        (name.startswith("snow") and "block" not in name),
+    ])
+
 
 def isIgnorableBlock(block):
     """
     :param block: name of the block to be checked
     :return: whether the block should be ignored during ray-casting
     """
-    name = block[10:]
+    name = block[10:]  # remove the "minecraft:" in front of the block's name
     if any([
-        name == "air",
-        name.startswith("snow"),
-        "obsidian" in name,
-        "nether" in name,
-        "portal" in name,
-        "_grass" in name,
-        "fern" in name,
-        "leaves" in name,
-        "_log" in name
+        name in ["air", "lily_pad"],
+        any([partialBlockName in name for partialBlockName in __partialBlockNames]),
+        __checkMoreSpecificConditions(name)
     ]):
         return True
     return False
